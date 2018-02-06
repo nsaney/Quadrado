91a35757e71845a309c04439f290c258c8c87189
Nicholas Saney <nsaney@gmail.com>
2018-02-05 23:13:27 -0500

    Added interface Comparable to EnumCodedObject.

--- code/quadrado-parent/quadrado-core/src/main/java/chairosoft/quadrado/game/resource/literals/EnumCodedObject.java
+++ code/quadrado-parent/quadrado-core/src/main/java/chairosoft/quadrado/game/resource/literals/EnumCodedObject.java
@@ -1,8 +1,14 @@
 package chairosoft.quadrado.game.resource.literals;
 
-public interface EnumCodedObject<E extends Enum<E> & EnumLiteral<E>> {
+public interface EnumCodedObject<E extends Enum<E> & EnumLiteral<E>> extends Comparable<EnumCodedObject<E>> {
     
-    ////// Instance Methods //////
+    ////// Instance Methods - Abstract //////
     E getCode();
+    
+    
+    ////// Instance Methods - Default //////
+    @Override default int compareTo(EnumCodedObject<E> that) {
+        return this.getCode().compareTo(that.getCode());
+    }
     
 }
