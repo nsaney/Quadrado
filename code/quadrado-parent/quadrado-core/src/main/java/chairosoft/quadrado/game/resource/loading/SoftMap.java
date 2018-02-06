12d44800ed2a60ce018d9d0527b85c2d9899f063
Nicholas Saney <nsaney@gmail.com>
2018-02-05 21:47:37 -0500

    Added thread safety to SoftMap class.

--- code/quadrado-parent/quadrado-core/src/main/java/chairosoft/quadrado/game/resource/loading/SoftMap.java
+++ code/quadrado-parent/quadrado-core/src/main/java/chairosoft/quadrado/game/resource/loading/SoftMap.java
@@ -1,6 +1,7 @@
 package chairosoft.quadrado.game.resource.loading;
 
 import java.lang.ref.SoftReference;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.Function;
 
@@ -20,16 +21,34 @@ public class SoftMap<K, V> {
     ////// Instance Methods //////
     public V get(K key) {
         V result = null;
-        SoftReference<V> softReference = this.innerMap.get(key);
-        if (softReference != null) {
-            result = softReference.get();
-        }
-        if (result == null) {
-            result = this.mappingFunction.apply(key);
-            SoftReference<V> freshSoftReference = new SoftReference<>(result);
-            this.innerMap.put(key, freshSoftReference);
+        synchronized (this.innerMap) {
+            SoftReference<V> softReference = this.innerMap.get(key);
+            if (softReference != null) {
+                result = softReference.get();
+            }
+            if (result == null) {
+                result = this.mappingFunction.apply(key);
+                SoftReference<V> freshSoftReference = new SoftReference<>(result);
+                this.innerMap.put(key, freshSoftReference);
+            }
+            this.purgeInvalidEntries();
         }
         return result;
+    }
+    
+    public void purgeInvalidEntries() {
+        synchronized (this.innerMap) {
+            ArrayList<K> invalidKeys = new ArrayList<>(this.innerMap.size());
+            for (K key : this.innerMap.keySet()) {
+                SoftReference<V> reference = this.innerMap.get(key);
+                if (reference.get() == null) {
+                    invalidKeys.add(key);
+                }
+            }
+            for (K key : invalidKeys) {
+                this.innerMap.remove(key);
+            }
+        }
     }
     
 }
