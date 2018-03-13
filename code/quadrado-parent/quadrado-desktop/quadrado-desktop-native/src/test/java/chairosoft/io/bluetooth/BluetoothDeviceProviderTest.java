17e713ee7ecf5f8399fbadbb2d14620ba856db5e
Nicholas Saney <nsaney@gmail.com>
2018-03-13 00:02:02 -0400

    Split C code into multiple maven profiles based on architecture and OS.

--- code/quadrado-parent/quadrado-desktop/quadrado-desktop-native/src/test/java/chairosoft/io/bluetooth/BluetoothDeviceProviderTest.java
+++ code/quadrado-parent/quadrado-desktop/quadrado-desktop-native/src/test/java/chairosoft/io/bluetooth/BluetoothDeviceProviderTest.java
@@ -3,10 +3,17 @@ package chairosoft.io.bluetooth;
 import chairosoft.quadrado.desktop.system.NarSystem;
 import org.junit.Test;
 
+import java.util.regex.Pattern;
+
 public class BluetoothDeviceProviderTest {
     
     @Test
     public void test_bluetoothDevice() {
+        System.out.printf(
+            "java.library.path = \n%s\n",
+            System.getProperty("java.library.path", "[no path found]")
+                .replaceAll(Pattern.quote(System.getProperty("path.separator", ":")), "\n")
+        );
         NarSystem.loadLibrary();
         BluetoothDeviceProvider.testBluetooth();
     }
