32beb7bbae4c17ac5ac9022181265f3275356873
Nicholas Saney <nsaney@gmail.com>
2018-04-03 20:50:28 -0400

    Added amd64 windows bluetooth test code.

--- code/quadrado-parent/quadrado-desktop/quadrado-desktop-native/src/test/java/chairosoft/io/bluetooth/BluetoothDeviceProviderTest.java
+++ code/quadrado-parent/quadrado-desktop/quadrado-desktop-native/src/test/java/chairosoft/io/bluetooth/BluetoothDeviceProviderTest.java
@@ -3,13 +3,14 @@ package chairosoft.io.bluetooth;
 import chairosoft.quadrado.desktop.system.NarSystem;
 import org.junit.Test;
 
+import java.io.PrintStream;
 import java.util.regex.Pattern;
 
 public class BluetoothDeviceProviderTest {
     
     @Test
     public void test_bluetoothDevice() {
-        System.err.printf(
+        System.out.printf(
             "java.library.path = \n%s\n",
             System.getProperty("java.library.path", "[no path found]")
                 .replaceAll(Pattern.quote(System.getProperty("path.separator", ":")), "\n")
@@ -19,7 +20,10 @@ public class BluetoothDeviceProviderTest {
             BluetoothDeviceProvider.testBluetooth();
         }
         catch (Exception ex) {
+            PrintStream stderr = System.err;
+            System.setErr(System.out);
             ex.printStackTrace();
+            System.setErr(stderr);
         }
     }
     
